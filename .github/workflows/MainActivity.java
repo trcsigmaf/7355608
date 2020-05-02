bf44c67ca682a727fbb793e7067e52ea78314660
trcsigmaf <59299132+trcsigmaf@users.noreply.github.com>
2020-05-02 14:57:17 -0400

    Add files via upload

--- .github/workflows/MainActivity.java
+++ .github/workflows/MainActivity.java
@@ -22,29 +22,28 @@ public class MainActivity extends AppCompatActivity
         button2 = findViewById(R.id.button1);
 
         //Set an onClickListener to detect clicks.
-        button1.setOnClickListener(new ButtonListener1());
-        button2.setOnClickListener(new ButtonListener2());
+        button1.setOnClickListener(new ButtonListener());
+        button2.setOnClickListener(new ButtonListener());
     }
 
-    class ButtonListener1 implements View.OnClickListener
+    class ButtonListener implements View.OnClickListener
     {
         @Override
         public void onClick(View v)
         {
+            String buttonText;
             //Create a new Intent from this activity to Page 2.
-            Intent intent = new Intent(MainActivity.this, Page2.class);
-            startActivity(intent);
-        }
-    }
-
-    class ButtonListener2 implements View.OnClickListener
-    {
-        @Override
-        public void onClick(View v)
-        {
-            //Create a new Intent from this activity to Page 3.
-            Intent intent = new Intent(MainActivity.this, Page3.class);
-            startActivity(intent);
+            buttonText = ((Button)v).getText().toString();
+            if(buttonText.equals("Driver"))
+            {
+                Intent intent = new Intent(MainActivity.this, Page2.class);
+                startActivity(intent);
+            }
+            else if(buttonText.equals("Customer"))
+            {
+                Intent intent = new Intent(MainActivity.this, Page3.class);
+                startActivity(intent);
+            }
         }
     }
 }
